4b973b3f0d1176643833f604f656b8ccb0cc3d5f
@Syeda Huma Jamshed <159490433+syedzadigithub@users.noreply.github.com>
2024-06-13 00:17:06 +0500

    project OOP My Bank
    This is my 10 project

---
+++ index.js
@@ -0,0 +1,112 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.greenBright `\n\t\t\t********************************WELCOME TO MY BANK*************************************`);
+// Bamk Account class
+class BankAccount {
+    accountNumber;
+    balance;
+    constructor(accountNumber, balance) {
+        (this.accountNumber = accountNumber), (this.balance = balance);
+    }
+    //   depit money
+    withDraw(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(chalk.gray(`\nWithdrawal of ${amount}$ successful. Remaining balance is ${this.balance}$.\n`));
+        }
+        else
+            console.log(chalk.red("\nInsufficiant balance.\n"));
+    }
+    //   credit money
+    deposit(amount) {
+        if (amount > 100) {
+            amount -= 1; // 1$ fee charge if more than 100$ is deposited
+        }
+        this.balance += amount;
+        console.log(chalk.blue(`\nDeposit of ${amount}$ successful. Remaining balance: ${this.balance}$.\n`));
+    }
+    // check balance
+    checkBalance() {
+        console.log(chalk.blue(`\nCurrent balance: ${this.balance}$.`));
+    }
+}
+//  Customer Class
+class Customer {
+    firstName;
+    lastName;
+    gender;
+    age;
+    mobileNumber;
+    account;
+    constructor(firstName, lastName, gender, age, mobileNumber, account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.account = account;
+    }
+}
+// Create bank accounts:
+let accounts = [
+    new BankAccount(10001, 2000),
+    new BankAccount(10002, 4500),
+    new BankAccount(10003, 5000),
+];
+// create customer 
+let customers = [
+    new Customer("Ahmed", "Hadi", "Male", 20, 3022187590, accounts[0]),
+    new Customer("Lubna", "Nazish", "Femain", 27, 3008938194, accounts[1]),
+    new Customer("Affan", "Shahmir", "Male", 28, 3113904009, accounts[2])
+];
+// Function to interact with bank account
+async function service() {
+    do {
+        let accountNumberInput = await inquirer.prompt({
+            name: "accountNumber",
+            type: "number",
+            message: "Enter your account number: "
+        });
+        let customer = customers.find(customer => customer.account.accountNumber === accountNumberInput.accountNumber);
+        if (customer) {
+            console.log(chalk.yellow(`\nWelcome ${customer.firstName} ${customer.lastName}!\n`));
+            let answer = await inquirer.prompt({
+                name: "select",
+                type: "list",
+                message: "Please select an operation: ",
+                choices: ["Deposit", "Withdraw", "Check Balance", "Exit"]
+            });
+            switch (answer.select) {
+                case "Deposit":
+                    let depositAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Please enter the amount to deposit: "
+                    });
+                    customer.account.deposit(depositAmount.amount);
+                    break;
+                case "Withdraw":
+                    let withDrawAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Please enter the amount to withdraw: "
+                    });
+                    customer.account.withDraw(withDrawAmount.amount);
+                    break;
+                case "Check Balance":
+                    customer.account.checkBalance();
+                    break;
+                case "Exit":
+                    console.log(chalk.red("Exiting bank program...."));
+                    console.log(chalk.yellow("\n Thank You for using our bank services. Have a great day!"));
+                    return;
+            }
+        }
+        else {
+            console.log(chalk.redBright(`Invalid account number. Please try again.`));
+        }
+    } while (true);
+}
+;
+service();
